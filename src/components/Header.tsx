86a660068d3cb1858308b970956d8fac59eec646
ieuan kinge <ieuan@ubik.studio>
2025-03-28 00:06:26 -0400

    first push to git

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+export function Header() {
+  return (
+    <header className="w-full">
+      <div className="container flex h-16 items-center">
+        <div className="text-xl font-medium text-black">aka ieuan</div>
+      </div>
+    </header>
+  )
+} 
